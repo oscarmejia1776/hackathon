c040cf2b517826ba1b1e92ed6fbf0bb33d5343d9
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-04-18 15:28:06 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log(bootstrap);
+
+// $("#submit").on("click", function () {
+//   const textValue = encodeURIComponent($("#userInput").val());
+//   $.get({
+//     url: `https://api.tvmaze.com/search/shows?q=${textValue}`,
+//     success: parseData,
+//     error: function (err) {
+//       console.log("something bad happenned");
+//     },
+//   });
+// });
+// $("#submit").on("click", function () {
+//   const textValue = encodeURIComponent($("#userInput").val());
+//   $.get({
+//     url: `https://api.tvmaze.com/search/shows?q=${textValue}`,
+//     success: parseData,
+//     error: function (err) {
+//       console.log("something bad happenned");
+//     },
+//   });
+// });
